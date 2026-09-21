f863c477ca40d63470a6fe0b3dbf58425f785f50
Mateusz Matuszak <mateusz_matuszak@hotmail.com>
2026-09-21 14:36:07 +0200

    update packages

--- init.sh
+++ init.sh
@@ -22,33 +22,26 @@ rm $MINIFORGE
 source $INSTALL_DIR/etc/profile.d/conda.sh
 
 ### === Create and activate environment ===
-echo "🧪 Creating conda environment 'foccus_ospar'..."
-conda create -y -n foccus_ospar python=3.12.2
-conda activate foccus_ospar
+echo "🧪 Creating conda environment 'foccus_traj'..."
+conda create -y -n foccus_traj python=3.12.2
+conda activate foccus_traj
 
 # Install mamba
 conda install -y -c conda-forge mamba
 
 ### === Install exact packages ===
 echo "📦 Installing required packages..."
+cd OpenDrift
+mamba install .
 mamba install -y -c conda-forge \
-  cartopy==0.25.0 \
-  cmocean==4.0.3 \
   ipython \
   ipywidgets==8.0.0 \
-  matplotlib==3.10.9 \
-  numpy==2.4.6 \
-  pandas==3.0.3 \
-  Shapely==2.1.2 \
-  xarray==2026.4.0 \
-  xugrid==0.15.2 \
-  openpyxl \
-  netcdf4 \
+  bokeh==3.10.0 \
   ipykernel jupyter nbformat nbconvert s3fs
 
 ### === Register kernel for Jupyter ===
 echo "🔗 Registering Jupyter kernel..."
-python -m ipykernel install --user --name foccus_ospar --display-name "Python (foccus_ospar)"
+python -m ipykernel install --user --name foccus_traj --display-name "Python (foccus_traj)"
 
 ### === Download notebook and helper script ===
 echo "📥 Downloading notebook and script..."
