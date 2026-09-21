1063c06699f272d9bca08e539720a0f775eb8e4c
Mateusz Matuszak <mateusz_matuszak@hotmail.com>
2026-09-21 14:28:23 +0200

    my files

--- init.sh
+++ init.sh
@@ -52,8 +52,8 @@ python -m ipykernel install --user --name foccus_ospar --display-name "Python (f
 
 ### === Download notebook and helper script ===
 echo "📥 Downloading notebook and script..."
-wget -N https://raw.githubusercontent.com/Deltares-research/EditoServices/main/FOCCUS_OSPAR_demonstrator/main.ipynb
-wget -N https://raw.githubusercontent.com/Deltares-research/EditoServices/main/FOCCUS_OSPAR_demonstrator/download_from_s3.py
+wget -N https://raw.githubusercontent.com/mateuszmatu/FOCCUS-trajectory-demonstrator/refs/heads/main/main.ipynb
+wget -N https://raw.githubusercontent.com/mateuszmatu/FOCCUS-trajectory-demonstrator/refs/heads/main/run_opendrift.py
 
 ### === Embed kernel metadata ===
 echo "⚙️ Embedding kernel metadata into notebook..."
@@ -79,43 +79,16 @@ jupyter nbconvert --clear-output --inplace main.ipynb
 echo "✅ Setup complete. You can now open main.ipynb and it will use the 'foccus_ospar' kernel by default."
 
 ### === Download input ===
-# Make folder
-mkdir -p data
-cd data
-
-# Base path to raw files on GitHub
-BASE_URL="https://github.com/Deltares-research/EditoServices/raw/main/FOCCUS_OSPAR_demonstrator/data"
-
-# List of files to download
-FILES=(
-  COMP4_assessment_areas_v8a.cpg
-  COMP4_assessment_areas_v8a.dbf
-  COMP4_assessment_areas_v8a.sbn
-  COMP4_assessment_areas_v8a.sbx
-  COMP4_assessment_areas_v8a.shp
-  COMP4_assessment_areas_v8a.shx
-  OSPAR_metrics_EQR_2015_2020.nc
-  River_perTarea_Feb.xlsx
-)
-
-# Download each file
-for file in "${FILES[@]}"; do
-  echo "Downloading $file..."
-  wget -nc "$BASE_URL/$file"
-done
-
-cd ..
 
 # Make folder
 mkdir -p imgs
 cd imgs
 
 # Base path to raw files on GitHub
-BASE_URL="https://github.com/Deltares-research/EditoServices/raw/main/FOCCUS_OSPAR_demonstrator/imgs"
+BASE_URL="https://github.com/mateuszmatu/FOCCUS-trajectory-demonstrator/tree/main/imgs"
 
 # List of files to download
 FILES=(
-  DF111.pdf
   FOCCUS_LOGO.jpg
 )
 
