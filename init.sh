7d90695c81d6cb336737544c890d5b33a57762cb
Mateusz Matuszak <mateusz_matuszak@hotmail.com>
2026-09-21 13:50:04 +0200

    revert init

--- init.sh
+++ init.sh
@@ -46,22 +46,14 @@ mamba install -y -c conda-forge \
   netcdf4 \
   ipykernel jupyter nbformat nbconvert s3fs
 
-# Install mamba
-conda install -y -c conda-forge mamba
-
-### === Install exact packages ===
-echo "📦 Installing additional packages..."
-mamba install -y -c conda-forge \
-    bohek
-
 ### === Register kernel for Jupyter ===
 echo "🔗 Registering Jupyter kernel..."
 python -m ipykernel install --user --name foccus_ospar --display-name "Python (foccus_ospar)"
 
 ### === Download notebook and helper script ===
 echo "📥 Downloading notebook and script..."
-wget -N https://raw.githubusercontent.com/mateuszmatu/FOCCUS-trajectory-demonstrator/refs/heads/main/main.ipynb
-wget -N https://raw.githubusercontent.com/mateuszmatu/FOCCUS-trajectory-demonstrator/refs/heads/main/run_opendrift.py
+wget -N https://raw.githubusercontent.com/Deltares-research/EditoServices/main/FOCCUS_OSPAR_demonstrator/main.ipynb
+wget -N https://raw.githubusercontent.com/Deltares-research/EditoServices/main/FOCCUS_OSPAR_demonstrator/download_from_s3.py
 
 ### === Embed kernel metadata ===
 echo "⚙️ Embedding kernel metadata into notebook..."
@@ -87,16 +79,43 @@ jupyter nbconvert --clear-output --inplace main.ipynb
 echo "✅ Setup complete. You can now open main.ipynb and it will use the 'foccus_ospar' kernel by default."
 
 ### === Download input ===
+# Make folder
+mkdir -p data
+cd data
+
+# Base path to raw files on GitHub
+BASE_URL="https://github.com/Deltares-research/EditoServices/raw/main/FOCCUS_OSPAR_demonstrator/data"
+
+# List of files to download
+FILES=(
+  COMP4_assessment_areas_v8a.cpg
+  COMP4_assessment_areas_v8a.dbf
+  COMP4_assessment_areas_v8a.sbn
+  COMP4_assessment_areas_v8a.sbx
+  COMP4_assessment_areas_v8a.shp
+  COMP4_assessment_areas_v8a.shx
+  OSPAR_metrics_EQR_2015_2020.nc
+  River_perTarea_Feb.xlsx
+)
+
+# Download each file
+for file in "${FILES[@]}"; do
+  echo "Downloading $file..."
+  wget -nc "$BASE_URL/$file"
+done
+
+cd ..
 
 # Make folder
 mkdir -p imgs
 cd imgs
 
 # Base path to raw files on GitHub
-BASE_URL="https://github.com/mateuszmatu/FOCCUS-trajectory-demonstrator/imgs"
+BASE_URL="https://github.com/Deltares-research/EditoServices/raw/main/FOCCUS_OSPAR_demonstrator/imgs"
 
 # List of files to download
 FILES=(
+  DF111.pdf
   FOCCUS_LOGO.jpg
 )
 
